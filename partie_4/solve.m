1611d7efd8be9288b970797531977cb4b2a29a0a
Thomas <>
2017-11-30 19:18:58 +0100

    Fonctionsolve fonctionnelle

--- partie_4/solve.m
+++ partie_4/solve.m
@@ -1,15 +1,12 @@
 function [g]=solve(g,n)
 
 A = build_A(g);
-P = eye(g.Nx);
-P(1,1) = 0;
-P(g.Nx,g.Nx) = 0;
+P = sparse(eye(g.Nx));
+P([1,g.Nx],:)=[];
 Ap = P * A * P';
-[~,Dp] = eigs(Ap - diag((max(g.indice))^2), n, 'sm'); %renvoie les n plus petites valeurs propres de la matrice
-lambda_p = diag(Dp) + max(g.indice)^2;
+[~,Dp] = eigs(Ap - ((max(g.indice))^2)*eye(size(Ap,2)), n, 'sm'); %renvoie les n plus petites valeurs propres de la matrice
+lambda_p = diag(Dp) + max(g.indice)^2
 neff = sqrt(lambda_p)
-
 g.neff = neff;
-
 
 end
