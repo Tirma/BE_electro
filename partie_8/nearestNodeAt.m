fded9565c9f5c998872f133987db2951099af942
Thomas <>
2017-12-28 18:06:01 +0100

    pic

---
+++ partie_8/nearestNodeAt.m
@@ -0,0 +1,100 @@
+
+function [id, dist]=nearestNodeAt(g, x0, y0)
+%
+% [id, dist]=nearestNodeAt(g, x0, y0)
+% liste des NV=4 proches voisins et leur distance à (x0, y0)
+%
+NV=4;
+[neighborIds, neighborDistances] = smallkNearestNeighbors(g.p, [x0 y0], NV);
+id=neighborIds;
+dist=neighborDistances;
+end
+
+function [neighborIds, neighborDistances] = smallkNearestNeighbors(dataMatrix, queryMatrix, k)
+%--------------------------------------------------------------------------
+% Program to find the k - nearest neighbors (kNN) within a set of points. 
+% Distance metric used: Euclidean distance
+% 
+% Usage:
+% [neighbors distances] = kNearestNeighbors(dataMatrix, queryMatrix, k);
+% dataMatrix  (N x D) - N vectors with dimensionality D (within which we search for the nearest neighbors)
+% queryMatrix (M x D) - M query vectors with dimensionality D
+% k           (1 x 1) - Number of nearest neighbors desired
+% 
+% Example:
+% a = [1 1; 2 2; 3 2; 4 4; 5 6];
+% b = [1 1; 2 1; 6 2];
+% [neighbors distances] = kNearestNeighbors(a,b,2);
+% 
+% Output:
+% neighbors =
+%      1     2
+%      1     2
+%      4     3
+% 
+% distances =
+%          0    1.4142
+%     1.0000    1.0000
+%     2.8284    3.0000
+%--------------------------------------------------------------------------
+
+neighborIds = zeros(size(queryMatrix,1),k);
+neighborDistances = neighborIds;
+
+numDataVectors = size(dataMatrix,1);
+numQueryVectors = size(queryMatrix,1);
+
+for i=1:numQueryVectors,
+    dist = sum((repmat(queryMatrix(i,:),numDataVectors,1)-dataMatrix).^2,2);
+    sortval=zeros(1, k);
+    for j=1:k,
+        [sortval(j),ind] = min(dist);
+        sortpos(j)=ind;
+        dist(ind) = +Inf ;
+    end
+    neighborIds(i,:) = sortpos(1:k);
+    neighborDistances(i,:) = sqrt(sortval(1:k));
+end
+end
+
+
+function [neighborIds neighborDistances] = kNearestNeighbors(dataMatrix, queryMatrix, k)
+%--------------------------------------------------------------------------
+% Program to find the k - nearest neighbors (kNN) within a set of points. 
+% Distance metric used: Euclidean distance
+% 
+% Usage:
+% [neighbors distances] = kNearestNeighbors(dataMatrix, queryMatrix, k);
+% dataMatrix  (N x D) - N vectors with dimensionality D (within which we search for the nearest neighbors)
+% queryMatrix (M x D) - M query vectors with dimensionality D
+% k           (1 x 1) - Number of nearest neighbors desired
+% 
+% Example:
+% a = [1 1; 2 2; 3 2; 4 4; 5 6];
+% b = [1 1; 2 1; 6 2];
+% [neighbors distances] = kNearestNeighbors(a,b,2);
+% 
+% Output:
+% neighbors =
+%      1     2
+%      1     2
+%      4     3
+% 
+% distances =
+%          0    1.4142
+%     1.0000    1.0000
+%     2.8284    3.0000
+%--------------------------------------------------------------------------
+
+neighborIds = zeros(size(queryMatrix,1),k);
+neighborDistances = neighborIds;
+
+numDataVectors = size(dataMatrix,1);
+numQueryVectors = size(queryMatrix,1);
+for i=1:numQueryVectors,
+    dist = sum((repmat(queryMatrix(i,:),numDataVectors,1)-dataMatrix).^2,2);
+    [sortval sortpos] = sort(dist,'ascend');
+    neighborIds(i,:) = sortpos(1:k);
+    neighborDistances(i,:) = sqrt(sortval(1:k));
+end
+end
